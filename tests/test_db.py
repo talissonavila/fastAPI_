1c16ded355e5174bc3604a352041dea7fb7c6be1
Talisson Avila <devtalisson@gmail.com>
2024-11-09 14:14:54 -0300

    criando crud para rota de todos

--- tests/test_db.py
+++ tests/test_db.py
@@ -2,7 +2,7 @@ from dataclasses import asdict
 
 from sqlalchemy import select
 
-from fastapi_zero.models import User
+from fastapi_zero.models import Todo, User
 
 
 def test_create_user(session, mock_db_time):
@@ -24,4 +24,22 @@ def test_create_user(session, mock_db_time):
         'email': 'avila@email.com',
         'created_at': time,
         'updated_at': time,
+        'todos': [],
     }
+
+
+def test_create_todo(session, user: User):
+    todo = Todo(
+        title='Test Todo',
+        description='Test Desc',
+        state='draft',
+        user_id=user.id,
+    )
+
+    session.add(todo)
+    session.commit()
+    session.refresh(todo)
+
+    user = session.scalar(select(User).where(User.id == user.id))
+
+    assert todo in user.todos
